6447ee60ff888f4cc0322a58ac764b32feea6e6b
Соловьенко Никита <nikidan6032638@yandex.ru>
2025-06-27 15:31:41 +0300

    add checkstyle

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.user;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * TODO Sprint add-controllers.
